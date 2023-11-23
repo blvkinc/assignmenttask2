c6f83886a3c8c0d70375e450b36e5fb181023070
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-11-22 17:50:32 -0800

    MultiLingualStringTable.java refactored code and updated tests

--- MultiLingualStringTableTest.java
+++ MultiLingualStringTableTest.java
@@ -1,21 +1,30 @@
+package base;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MultiLingualStringTableTest {
 
     @Test
-    void testGetMessageEnglish() {
-        MultiLingualStringTable.cl = MultiLingualStringTable.LanguageSetting.English;
-        assertEquals("Enter your name:", MultiLingualStringTable.getMessage(0));
-        assertEquals("Welcome", MultiLingualStringTable.getMessage(1));
-        assertEquals("Have a good time playing Abominodo", MultiLingualStringTable.getMessage(2));
+    void testGetMessageInEnglish() {
+        MultiLingualStringTable.LanguageSetting language = MultiLingualStringTable.LanguageSetting.ENGLISH;
+
+        MultiLingualStringTable.getMessage(0);
+        String expected = "Enter your name:";
+        String actual = MultiLingualStringTable.getMessage(0);
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    void testGetMessageKlingon() {
-        MultiLingualStringTable.cl = MultiLingualStringTable.LanguageSetting.Klingon;
-        assertEquals("'el lIj pong:", MultiLingualStringTable.getMessage(0));
-        assertEquals("nuqneH", MultiLingualStringTable.getMessage(1));
-        assertEquals("QaQ poH Abominodo", MultiLingualStringTable.getMessage(2));
+    void testGetMessageInKlingon() {
+        MultiLingualStringTable.LanguageSetting language = MultiLingualStringTable.LanguageSetting.KLINGON;
+
+        MultiLingualStringTable.getMessage(1);
+        String expected = "nuqneH";
+        String actual = MultiLingualStringTable.getMessage(1);
+
+        assertEquals(expected, actual);
     }
 }
