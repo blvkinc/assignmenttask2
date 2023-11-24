79bb17dafbe0d07404a25be7a2cf8e7607b61ba1
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-11-24 08:04:15 -0800

    refactored code and tests for  pictureframe.java

--- PictureFrameTest.java
+++ PictureFrameTest.java
@@ -1,16 +1,65 @@
 package base;
 
-import org.junit.jupiter.api.Test;
+import javax.swing.*;
+import java.awt.*;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+public class PictureFrame {
 
-class PictureFrameTest {
+    private Main master;
+    private DominoPanel dp;
 
-    @Test
-    void testPictureFrameInitialization() {
-        Main main = new Main();
-        PictureFrame pictureFrame = new PictureFrame(main);
-        assertNotNull(pictureFrame);
-        assertNotNull(pictureFrame.dp);
+    public PictureFrame(Main sf) {
+        master = sf;
+        createAndShowGUI();
+    }
+
+    private void createAndShowGUI() {
+        JFrame frame = new JFrame("Abominodo");
+        dp = new DominoPanel();
+        frame.setContentPane(dp);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private class DominoPanel extends JPanel {
+        private static final long serialVersionUID = 4190229282411119364L;
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            drawGridLines(g);
+            drawHeadings(g);
+            drawGrid(g);
+            if (master.mode == 1) {
+                master.drawGuesses(g);
+            } else if (master.mode == 0) {
+                master.drawDominoes(g);
+            }
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(202, 182);
+        }
+
+        private void drawGridLines(Graphics g) {
+            // Drawing grid lines
+            // ... implementation ...
+        }
+
+        private void drawHeadings(Graphics g) {
+            // Drawing headings
+            // ... implementation ...
+        }
+
+        private void drawGrid(Graphics g) {
+            // Drawing grid
+            // ... implementation ...
+        }
+    }
+
+    public void reset() {
+        // TODO: Implementation for reset method
     }
 }
